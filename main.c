73703829d6e26ba189a46aa4d1c9d7b1ed1b7db6
kitworker <kasyanov@gmail.com>
2015-05-24 18:59:22 +0400

    my first  commit c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "lib/stm32f0xx.h"
+#include "lib/system_stm32f0xx.h"
+#include "gpio/gpio.h"
+#include "delay/delay.h"
+#include "stm32f0-discovery.h"
+#include "lcd3310/lcd3310.h"
+
+int main( void )
+{
+    SystemInit();
+    delay_init( 48 );
+    lcd3310_init();
+    lcd3310_string( "tekst", 0 , 0 );
+    lcd3310_value_float( 3.14, 0, 1 );
+    lcd3310_value_integer( 3255, 0, 2 );
+
+    for(;;)
+    {
+
+    }
+
+	return 0;
+}
+
